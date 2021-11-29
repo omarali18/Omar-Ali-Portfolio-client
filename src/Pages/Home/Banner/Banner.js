ade96121d2de7490725f51023d35a79076c74625
omarali18 <omarali1814000@gmail.com>
2021-11-29 18:38:43 +0600

    banner added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import banner from "../../../Images/banner.jpg"
+import "./Banner.css"
 
 const Banner = () => {
     return (
-        <div className="bg-info">
+        <div className="banner-container">
             <div class="row">
                 <div class="col-12 col-md-6">
-                    Column
+                    <div className="d-flex flex-column  align-items-center banner-detail">
+                        <h1>I'm Omar Ali</h1>
+                        <h2>Full Stack Web Developer</h2>
+                        <a href="https://drive.google.com/file/d/18ucOHrsrx-eWiqWq6fcDdlMNLNN9rNMR/view?usp=sharing" target="blank"><button className="banner-resume">Download Resume</button></a>
+                    </div>
                 </div>
                 <div class="col-12 col-md-6">
                     <img src={banner} className="img-fluid" alt="" />
